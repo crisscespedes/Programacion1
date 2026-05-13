54bee9280601413df82c74b30f37f90454941b2c
crisscespedes <crisscespedespardo@gmail.com>
2026-05-12 21:56:36 -0400

    Nuevo ejercicio agregado n2_par

---
+++ segundo_bimestre/n2_par.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+
+    cout<<"Ingrese dos numeros :"<<endl;
+    cin>>n1>>n2;
+
+    if (n1%2==0)
+    {
+        if (n2%2==0)
+        {
+            cout<<"Ambos son pares";
+        }
+        
+    }
+    
+}
