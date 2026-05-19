6a479608675aec07493295f44d2db3521cad3f5f
crisscespedes <crisscespedespardo@gmail.com>
2026-05-19 18:43:47 -0400

    Cambio endl por \n para salto de linea

--- segundo_bimestre/n_ap_rep.cpp
+++ segundo_bimestre/n_ap_rep.cpp
@@ -6,19 +6,20 @@ int main ()
     float n1,n2,n3,n4;
     float p_notas;
 
-    cout<<"Ingrese 4 notas:"<<endl;
+    cout<<"Ingrese 4 notas: \n";
     cin>>n1>>n2>>n3>>n4;
 
     
     p_notas = (n1+n2+n3+n4)/4;
 
    
-    
+   
     
     if (p_notas>=61)
     {
-        cout<<"Aprobado"<<endl;
+        cout<<"Aprobado\n";
     }
     else
-        cout<<"Reprobado"<<endl;
+        cout<<"Reprobado\n";
+    return 0;
 }
