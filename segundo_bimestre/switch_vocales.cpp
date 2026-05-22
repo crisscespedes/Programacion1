442f3ac8954b1eba6b93a150584c0512dbdb651e
crisscespedes <crisscespedespardo@gmail.com>
2026-05-21 21:44:03 -0400

    Nuevos ejercicios agregados

--- segundo_bimestre/switch_vocales.cpp
+++ segundo_bimestre/switch_vocales.cpp
@@ -10,18 +10,24 @@ int main()
     switch (vocal)
     {
     case 'a':
+    case 'A':
         cout<<"Es la vocal a";
         break;
+        
     case 'e':
+    case 'E':
         cout<<"Es la vocal e";
         break;
     case 'i':
+    case 'I':
         cout<<"Es la vocal i";
         break;        
     case 'o':
+    case 'O':
         cout<<"Es la vocal o";
         break;
     case 'u':
+    case 'U':
         cout<<"Es la vocal u";
         break;    
 
